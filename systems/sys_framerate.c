e25e9ca142a968a54f1af21fbe114ecfbfe7f56c
Staś Małolepszy <stas@duzodobrze.pl>
2019-11-11 17:15:54 +0100

    Use snprintf rather than sprintf_s

--- systems/sys_framerate.c
+++ systems/sys_framerate.c
@@ -1,9 +1,10 @@
 #include <SDL2/SDL.h>
+#include <stdio.h>
 #include "../game.h"
 
 void sys_framerate(Game* game, float delta)
 {
 	char fps[20];
-	sprintf_s(fps, 20, "Frame: %4.3fs", delta);
+	snprintf(fps, 20, "Frame: %4.3fs", delta);
 	SDL_SetWindowTitle(game->window, fps);
 }
