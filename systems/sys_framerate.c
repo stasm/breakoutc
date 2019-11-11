49cd9db42506ca951de3354baad9c304706e49c6
Staś Małolepszy <stas@duzodobrze.pl>
2019-11-11 11:06:54 +0100

    (sys_framerate) Use sprintf_s

--- systems/sys_framerate.c
+++ systems/sys_framerate.c
@@ -4,6 +4,6 @@
 void sys_framerate(Game* game, float delta)
 {
 	char fps[20];
-	sprintf(fps, "Frame: %4.3fs", delta);
+	sprintf_s(fps, 20, "Frame: %4.3fs", delta);
 	SDL_SetWindowTitle(game->window, fps);
 }
